ae2b5d036f278f2a502ae63a508d874e3da80ef2
Bob Haring <haring@evident.nl>
2022-01-23 11:35:18 +0100

    Update extension to manifest V3

--- js/background/index.js
+++ js/background/index.js
@@ -1,54 +1,58 @@
-import { getEnvironment, isActive } from '../matchUrls.js'
-import { environments } from '../../data/siteEnvironmentMapping.js'
+import { getEnvironment, isActive } from "../matchUrls.js";
+import { environments } from "../../data/siteEnvironmentMapping.js";
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    // Disable if is not an url meant for the extension
-    disableExtension(tab)
+	// Disable if is not an url meant for the extension
+	disableExtension(tab);
 
-    // Add environment badge
-    setEnvironmentBadge(tab)
-})
+	// Add environment badge
+	setEnvironmentBadge(tab);
+});
 
 chrome.tabs.onActivated.addListener(activeInfo => {
-    chrome.tabs.get(activeInfo.tabId, tab => {
-        // Disable if is not an url meant for the extension
-        disableExtension(tab)
+	chrome.tabs.get(activeInfo.tabId, tab => {
+		// Disable if is not an url meant for the extension
+		disableExtension(tab);
 
-        // Add environment badge
-        setEnvironmentBadge(tab)
-    })
-})
+		// Add environment badge
+		setEnvironmentBadge(tab);
+	});
+});
 
 const setEnvironmentBadge = tab => {
-    let environment = getEnvironment(tab.url)
+	let environment = getEnvironment(tab.url);
 
-    let badgeColor = "#000"
+	if (!environment) {
+		return;
+	}
 
-    switch (environment) {
-        case environments.prd:
-            badgeColor = "#ff0000"
-            break;
-        case environments.acc:
-            badgeColor = "#002BC6"
-            break;
-        case environments.tst:
-        case environments.dev:
-            badgeColor = "#00ab66"
-            break;
-        default:
-            badgeColor = "#000"
-    }
-    
-    chrome.browserAction.setBadgeText({tabId: tab.id, text: environment})
-    chrome.browserAction.setBadgeBackgroundColor({color: badgeColor})
-}
+	let badgeColor = "#000";
 
-const disableExtension = (tab) => {
-    if (!isActive(tab.url)) {
-        chrome.browserAction.setIcon({ path: "../../icons/swap128x128-no-match.png"})
+	switch (environment) {
+		case environments.prd:
+			badgeColor = "#ff0000";
+			break;
+		case environments.acc:
+			badgeColor = "#002BC6";
+			break;
+		case environments.tst:
+		case environments.dev:
+			badgeColor = "#00ab66";
+			break;
+		default:
+			badgeColor = "#000";
+	}
 
-        chrome.browserAction.disable(tab.id)
-    } else {
-        chrome.browserAction.setIcon({ path: "../../icons/swap128x128.png"})
-    }
-}
+	chrome.action.setBadgeText({ tabId: tab.id, text: environment });
+	chrome.action.setBadgeBackgroundColor({ color: badgeColor });
+};
+
+const disableExtension = tab => {
+	if (!isActive(tab.url)) {
+		chrome.action.setIcon({ path: "../../icons/swap128x128-no-match.png" });
+
+		chrome.action.disable(tab.id);
+	} else {
+		chrome.action.setIcon({ path: "../../icons/swap128x128.png" });
+	}
+};
